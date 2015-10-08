5c9de3f1f1987724fffab8e8d5684888b9f837e1
Kristi <kpillet1@gmail.com>
2015-10-08 21:37:02 +0300

    kmmi saadetud kood

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,22 +1,29 @@
 package program;
 
-/**
- * Created by tonis on 10/8/15.
- */
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class Main {
-    public static Object HelloWorld() {
-        System.out.println("hi");
-        return 1;
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL("http://193.40.243.200/soidutaja/");
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+            String lammas = buffer.toString();
+            JSONObject jsonObj = new JSONObject(lammas);
+            System.out.println(jsonObj);
+        } finally {
+            if (reader != null)
+                reader.close();
+        }
     }
-
-    public static Object GetLocations() {
-        return true;
-    }
-
-    public static Object GetURL() {
-        return false;
-    }
-
-    //lammas
 }
