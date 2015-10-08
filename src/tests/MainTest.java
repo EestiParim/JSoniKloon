29d22fb68a8f7574ee82aad0e9059a6bd3cbc380
Kristi <kpillet1@gmail.com>
2015-10-08 21:57:49 +0300

    testid

---
+++ src/tests/MainTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kristi on 8.10.2015.
+ */
+public class MainTest {
+
+    }
+
